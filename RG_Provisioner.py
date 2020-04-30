69a8f925cd36f4c3e26e4670bce2a874003b3beb
GH95 <georgehill0040@yahoo.co.uk>
2020-04-30 16:09:51 +0100

    Enhancements
    - Remove use of prefix variable
    - Rename resource-name to vnet name, and use vnet name explicitly to name vnets
    - Explicitly only allow the use of one service endpoint per created vpc

--- RG_Provisioner.py
+++ RG_Provisioner.py
@@ -10,10 +10,6 @@ def parse_arguments():
                         dest="location",
                         default="uksouth",
                         required=False)
-    parser.add_argument("--prefix",
-                        help="The prefix to add to the resource group name",
-                        dest="prefix",
-                        required=True)
     parser.add_argument("--resource-name",
                         help="The name of the resource you wish to create",
                         dest="resource_name",
@@ -25,18 +21,18 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def check_if_resource_group_exists(prefix, resource_name):
-    check_if_resource_group_exists_command = ["group", "exists", "-n", "{}{}RG".format(prefix, resource_name)]
+def check_if_resource_group_exists(resource_name):
+    check_if_resource_group_exists_command = ["group", "exists", "-n", "{}RG".format(resource_name)]
     resource_group_exists = execute_az_command(command_to_execute=check_if_resource_group_exists_command)
 
     if resource_group_exists:
-        raise Exception("Resource Group {}{}RG Already Exists, Exiting...".format(prefix, resource_name))
+        raise Exception("Resource Group {}RG Already Exists, Exiting...".format(resource_name))
 
 
-def create_resource_group(location, prefix, resource_name, subscription):
-    check_if_resource_group_exists(prefix=prefix, resource_name=resource_name)
+def create_resource_group(location, resource_name, subscription):
+    check_if_resource_group_exists(resource_name=resource_name)
     create_resouce_group_command = ["group", "create", "-l", "{}".format(location), "-n",
-                                    "{}{}RG".format(prefix, resource_name), "--tags", "Component={}".format(resource_name),
+                                    "{}RG".format(resource_name), "--tags", "Component={}".format(resource_name),
                                     "--subscription", "{}".format(subscription)]
     execute_az_command(command_to_execute=create_resouce_group_command)
 
@@ -44,7 +40,6 @@ def create_resource_group(location, prefix, resource_name, subscription):
 if __name__ == '__main__':
     args = parse_arguments()
     location = args.location
-    prefix = args.prefix
     resource_name = args.resource_name
     subscription = args.subscription
-    create_resource_group(location=location, prefix=prefix, resource_name=resource_name, subscription=subscription)
+    create_resource_group(location=location, resource_name=resource_name, subscription=subscription)
--- VNET_Provisioner.py
+++ VNET_Provisioner.py
@@ -15,71 +15,74 @@ def parse_arguments():
                         dest="location",
                         default="uksouth",
                         required=False)
-    parser.add_argument("--prefix",
-                        help="The prefix to add to the resource group name",
-                        dest="prefix",
-                        required=True)
-    parser.add_argument("--resource-name",
-                        help="The name of the resource you wish to create",
-                        dest="resource_name",
+    parser.add_argument("--resource-group",
+                        help="The resource group in which to create the vnet",
+                        dest="resource_group",
                         required=True)
     parser.add_argument("--subnet-range",
                         help="The subnet range to use (e.g. 10.0.0.0/24)",
                         dest="subnet_range",
                         required=True)
-    parser.add_argument("--service-endpoints",
-                        help="The list of service endpoints to connect the VNet (must be comma separated)",
-                        dest="service_endpoints",
-                        default="Microsoft.AzureActiveDirectory",
-                        type=str,
+    parser.add_argument("--service-endpoint",
+                        help="The service endpoint to connect the VNet",
+                        dest="service_endpoint",
                         required=False)
     parser.add_argument("--subscription",
                         help="The name of the subscription in which to create the resource",
                         dest="subscription",
                         required=True)
+    parser.add_argument("--vnet-name",
+                        help="The name of the vnet you wish to create",
+                        dest="vnet_name",
+                        required=True)
     return parser.parse_args()
 
 
-def check_if_vnet_exists(prefix, resource_name):
+def create_vnet(ipv4_range, location, resource_group, service_endpoint, subnet_range, subscription, vnet_name):
+    check_if_vnet_exists(vnet_name=vnet_name)
+    create_vnet_command = ["network", "vnet", "create", "--name", "{}".format(vnet_name),
+                           "--resource-group", "{}".format(resource_group), "--location",
+                           "{}".format(location), "--address-prefix", "{}".format(ipv4_range), "--subnet-name",
+                           "{}Subnet".format(vnet_name), "--subnet-prefix", "{}".format(subnet_range), "--tags",
+                           "Component={}".format(vnet_name), "--subscription", "{}".format(subscription)]
+    execute_az_command(command_to_execute=create_vnet_command)
+    if service_endpoint:
+        connect_vnet_to_service_endpoint(resource_group=resource_group, service_endpoint=service_endpoint,
+                                         subscription=subscription, vnet_name=vnet_name)
+
+
+def check_if_vnet_exists(vnet_name):
     vnet_list_command = ["network", "vnet", "list"]
     vnet_list = execute_az_command(command_to_execute=vnet_list_command)
     if vnet_list:
         for vnet_record in vnet_list:
-            vnet_name = vnet_record['name']
-            if vnet_name == "{}{}VNet".format(prefix, resource_name):
-                raise Exception("VNET name {} already exists".format(vnet_name))
+            vnet_name_record = vnet_record['name']
+            if vnet_name_record == "{}".format(vnet_name):
+                raise Exception("VNET name {} already exists".format(vnet_name_record))
     else:
         print("No VNets found, continuing...")
 
 
-def connect_vnet_to_service_endpoint(prefix, resource_name, service_endpoints, subscription):
+def connect_vnet_to_service_endpoint(resource_group, service_endpoint, subscription, vnet_name):
     service_endpoint_connector_command = ["network", "vnet", "subnet", "update", "--resource-group",
-                                          "{}{}RG".format(prefix, resource_name), "--name",
-                                          "{}{}Subnet".format(prefix, resource_name), "--vnet-name",
-                                          "{}{}".format(prefix, resource_name), "--subscription",
-                                          "{}".format(subscription)]
+                                          "{}".format(resource_group), "--name",
+                                          "{}Subnet".format(vnet_name), "--vnet-name",
+                                          "{}".format(vnet_name), "--subscription",
+                                          "{}".format(subscription), "--service-endpoints",
+                                          service_endpoint]
+    print(service_endpoint_connector_command)
     execute_az_command(command_to_execute=service_endpoint_connector_command)
-
-
-def create_vnet(ipv4_range, location, prefix, resource_name, service_endpoints, subnet_range, subscription):
-    check_if_vnet_exists(prefix=prefix, resource_name=resource_name)
-    create_vnet_command = ["network", "vnet", "create", "--name", "{}{}".format(prefix, resource_name),
-                           "--resource-group", "{}{}RG".format(prefix, resource_name), "--location",
-                           "{}".format(location), "--address-prefix", "{}".format(ipv4_range), "--subnet-name",
-                           "{}{}Subnet".format(prefix, resource_name), "--subnet-prefix",
-                           "{}".format(subnet_range), "--tags", "Component={}".format(resource_name), "--subscription",
-                           "{}".format(subscription)]
-    execute_az_command(command_to_execute=create_vnet_command)
 
 
 if __name__ == '__main__':
     args = parse_arguments()
     ipv4_range = args.ipv4_range
     location = args.location
-    prefix = args.prefix
-    resource_name = args.resource_name
-    service_endpoints = args.service_endpoints
+    resource_group = args.resource_group
+    service_endpoint = args.service_endpoint
     subnet_range = args.subnet_range
     subscription = args.subscription
-    create_vnet(ipv4_range=ipv4_range, location=location, prefix=prefix, resource_name=resource_name,
-                service_endpoints=service_endpoints, subnet_range=subnet_range, subscription=subscription)
+    vnet_name = args.vnet_name
+    create_vnet(ipv4_range=ipv4_range, location=location,  service_endpoint=service_endpoint,
+                resource_group=resource_group, subnet_range=subnet_range, subscription=subscription,
+                vnet_name=vnet_name)
