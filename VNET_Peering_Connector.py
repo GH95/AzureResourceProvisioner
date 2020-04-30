97304c63c8f780d72fedb6fa6a41d2aa18b23ba9
GH95 <georgehill0040@yahoo.co.uk>
2020-04-30 10:40:44 +0100

    Update vnet peering connector to work for vnets in the same or multiple rgs

--- VNET_Peering_Connector.py
+++ VNET_Peering_Connector.py
@@ -7,17 +7,23 @@ def parse_arguments():
     description = 'Arguments for creating a new resource group'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("--primary-resource-group",
-                        help="The name of the resource group in which the primary vnet resides",
+                        help="The name of the resource group in which the primary vnet resides (for when the two vnets"
+                             "reside in two different resource groups)",
                         dest="primary_resource_group",
-                        required=True)
+                        required=False)
     parser.add_argument("--primary-vnet",
                         help="The VNet you wish to create the peering from",
                         dest="primary_vnet",
                         required=True)
+    parser.add_argument("--resource-group",
+                        help="The name of the resource group in which both vnets reside (both vnets must exist here)",
+                        dest="resource_group",
+                        required=False)
     parser.add_argument("--secondary-resource-group",
-                        help="The name of the resource group in which the secondary vnet resides",
+                        help="The name of the resource group in which the secondary vnet resides (for when the two vnets"
+                             "reside in two different resource groups)",
                         dest="secondary_resource_group",
-                        required=True)
+                        required=False)
     parser.add_argument("--secondary-vnet",
                         help="The VNet you wish to create the peering to",
                         dest="secondary_vnet",
@@ -26,6 +32,12 @@ def parse_arguments():
                         help="The name of the subscription in which to create the resource",
                         dest="subscription",
                         required=True)
+    parser.add_argument("--vnets-in-same-rg",
+                        help="True if vnets exist in same resource groups, false if not",
+                        choices=["True", "False"],
+                        default="False",
+                        dest="vnets_in_same_rg",
+                        required=False)
     return parser.parse_args()
 
 
@@ -58,26 +70,59 @@ def create_vnet_peering(resource_group, subscription, target_vnet, target_vnet_r
     execute_az_command(command_to_execute=vnet_peering_command)
 
 
-if __name__ == '__main__':
-    args = parse_arguments()
-    primary_resource_group = args.primary_resource_group
-    primary_vnet = args.primary_vnet
-    secondary_resource_group = args.secondary_resource_group
-    secondary_vnet = args.secondary_vnet
-    subscription = args.subscription
-
-    # Get primary and secondary vnet resource IDs
-    primary_vnet_resource_id = get_vnet_resource_id(resource_group=primary_resource_group, vnet_name=primary_vnet)
-    secondary_vnet_resource_id = get_vnet_resource_id(resource_group=secondary_resource_group, vnet_name=secondary_vnet)
-
+def vnet_peering_connector_handler(primary_vnet, primary_vnet_resource_id, resource_group_a, resource_group_b,
+                                   secondary_vnet, secondary_vnet_resource_id, subscription):
     # For both resources, first check if the vnet peering exists
-    check_if_vnet_peering_exists(resource_group=primary_resource_group, subscription=subscription,
+    check_if_vnet_peering_exists(resource_group=resource_group_a, subscription=subscription,
                                  target_vnet=secondary_vnet, vnet=primary_vnet)
-    check_if_vnet_peering_exists(resource_group=secondary_resource_group, subscription=subscription,
+    check_if_vnet_peering_exists(resource_group=resource_group_b, subscription=subscription,
                                  target_vnet=primary_vnet, vnet=secondary_vnet)
 
     # Once confirmed that neither peering exists, create both peerings
-    create_vnet_peering(resource_group=primary_resource_group, subscription=subscription, target_vnet=secondary_vnet,
+    create_vnet_peering(resource_group=resource_group_a, subscription=subscription, target_vnet=secondary_vnet,
                         target_vnet_resource_id=secondary_vnet_resource_id, vnet=primary_vnet)
-    create_vnet_peering(resource_group=secondary_resource_group, subscription=subscription, target_vnet=primary_vnet,
+    create_vnet_peering(resource_group=resource_group_b, subscription=subscription, target_vnet=primary_vnet,
                         target_vnet_resource_id=primary_vnet_resource_id, vnet=secondary_vnet)
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    primary_vnet = args.primary_vnet
+    secondary_vnet = args.secondary_vnet
+    subscription = args.subscription
+    vnets_in_same_rg = args.vnets_in_same_rg
+    vnets_in_same_rg_response = False
+    if vnets_in_same_rg == "True":
+        vnets_in_same_rg_response = True
+
+    if not vnets_in_same_rg_response:
+        primary_resource_group = args.primary_resource_group
+        if not primary_resource_group:
+            raise Exception("Primary group argument must be supplied if vnets are in separate RGs")
+
+        secondary_resource_group = args.secondary_resource_group
+        if not secondary_resource_group:
+            raise Exception("Secondary group argument must be supplied if vnets are in separate RGs")
+
+        primary_vnet_resource_id = get_vnet_resource_id(resource_group=primary_resource_group, vnet_name=primary_vnet)
+        secondary_vnet_resource_id = get_vnet_resource_id(resource_group=secondary_resource_group,
+                                                          vnet_name=secondary_vnet)
+        vnet_peering_connector_handler(primary_vnet=primary_vnet,
+                                       primary_vnet_resource_id=primary_vnet_resource_id,
+                                       resource_group_a=primary_resource_group,
+                                       resource_group_b=secondary_resource_group,
+                                       secondary_vnet=secondary_vnet,
+                                       secondary_vnet_resource_id=secondary_vnet_resource_id,
+                                       subscription=subscription)
+    else:
+        try:
+            resource_group = args.resource_group
+        except AttributeError:
+            raise Exception("Resource group argument must be supplied if vnets are in the same RG")
+        vnet_peering_connector_handler(primary_vnet=primary_vnet,
+                                       primary_vnet_resource_id=primary_vnet,
+                                       resource_group_a=resource_group,
+                                       resource_group_b=resource_group,
+                                       secondary_vnet=secondary_vnet,
+                                       secondary_vnet_resource_id=secondary_vnet,
+                                       subscription=subscription)
